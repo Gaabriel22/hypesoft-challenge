22430dae20e1096520f8df7a6ca0278448f8b19a
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-09-03 11:11:24 -0300

    refactor(lib): use central queryClient in ReactQueryProvider

--- frontend/src/lib/reactQueryProvider.tsx
+++ frontend/src/lib/reactQueryProvider.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
-import { ReactNode, useState } from "react"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
+import { queryClient } from "../stores/queryClient"
 
 type Props = {
   children: ReactNode
 }
 
 export function ReactQueryProvider({ children }: Props) {
-  const [client] = useState(() => new QueryClient())
-
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
 }
